749691d26807e873824b8c0b1120190a289eba6a
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2022-12-17 10:46:41 +0200

    Initial Commit

---
+++ DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Oleksii Leshchenko on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
